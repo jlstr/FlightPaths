a9ee4b2a9de827d2d689cdfa83a0324902052c28
Jose Estrella Campana <jlecampana@gmail.com>
2025-05-23 16:59:57 +0100

    Add GraphBuilder Unit Tests

--- src/graph_builder.cpp
+++ src/graph_builder.cpp
@@ -6,9 +6,12 @@ using namespace std;
 Graph GraphBuilder::assemble(vector<vector<int>>& cost_matrix, vector<string>& cities) {
   Graph graph;
 
-  for (int i = 0; i < cost_matrix.size(); ++i)
-    for (int j = i + 1; j < cost_matrix[i].size(); ++j)        
-      graph[cities[i]].push_back({ cities[j], cost_matrix[i][j] });
+  for (int i = 0; i < cost_matrix.size(); ++i) {
+    for (int j = i + 1; j < cost_matrix[i].size(); ++j) {
+      if (cost_matrix[i][j] != 0)
+        graph[cities[i]].push_back({ cities[j], cost_matrix[i][j] });
+    }
+  }
 
   return graph;
 }
--- src/tests/graph_builder_test.cpp
+++ src/tests/graph_builder_test.cpp
@@ -1,9 +1,79 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "./../headers/graph_builder.h"
+using namespace std;
+using ::testing::ElementsAre;
+using ::testing::UnorderedElementsAre;
 
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
-  EXPECT_EQ(666, 666);
+class GraphBuilderTest: public ::testing::Test {
+protected:
+  vector<vector<int>> cost_matrix;
+  vector<string> cities;
+
+  void SetUp() override {
+    cost_matrix = {
+      { 0, 15, 80, 90 },
+      { 0,  0, 40, 50 },
+      { 0,  0,  0, 70 },
+      { 0,  0,  0,  0 },
+    };
+
+    cities = { "Castle Black", "Winterfell", "Riverrun", "King's Landing" };
+  }
+
+  Graph assemble() {
+    return GraphBuilder::assemble(cost_matrix, cities);
+  }
+};
+
+TEST_F(GraphBuilderTest, AssemblesCorrectGraphWithFourCities) {
+  Graph graph = assemble();
+
+  ASSERT_EQ(graph.size(), 3);
+
+  EXPECT_THAT(graph["Castle Black"], ElementsAre(
+    make_pair(string("Winterfell"), 15),
+    make_pair(string("Riverrun"), 80),
+    make_pair(string("King's Landing"), 90)
+  ));
+
+  EXPECT_THAT(graph["Winterfell"], ElementsAre(
+    make_pair(string("Riverrun"), 40),
+    make_pair(string("King's Landing"), 50)
+  ));
+
+  EXPECT_THAT(graph["Riverrun"], ElementsAre(
+    make_pair(string("King's Landing"), 70)
+  ));
+
+  EXPECT_EQ(graph.count("King's Landing"), 0);
+}
+
+TEST_F(GraphBuilderTest, EmptyInputProducesEmptyGraph) {
+  cost_matrix.clear();
+  cities.clear();
+
+  Graph graph = assemble();
+  EXPECT_TRUE(graph.empty());
+}
+
+TEST_F(GraphBuilderTest, SingleCityProducesEmptyGraph) {
+  cost_matrix = { { 0 } };
+  cities = { "Tokyo" };
+
+  Graph graph = assemble();
+  EXPECT_TRUE(graph.empty());
+}
+
+TEST_F(GraphBuilderTest, NoEdgesWhenMatrixIsZeroExceptDiagonal) {
+  cost_matrix = {
+    { 0, 0, 0 },
+    { 0, 0, 0 },
+    { 0, 0, 0 }
+  };
+
+  cities = { "Paris", "New York", "London" };
+
+  Graph graph = assemble();
+  EXPECT_TRUE(graph.empty());
 }
