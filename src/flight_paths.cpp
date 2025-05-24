89b2e7b73f84801a0c92b3f9c9ef006092dc7e12
Jose Estrella Campana <jlecampana@gmail.com>
2025-05-24 10:31:35 +0100

    Loading from File(s) draft

--- src/flight_paths.cpp
+++ src/flight_paths.cpp
@@ -1,19 +1,79 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 #include "./headers/graph_builder.h"
 #include "./headers/path_finder.h"
 using namespace std;
 
-int main() {
-  vector<vector<int>> cost_matrix = {
-    { 0, 15, 80, 90 },
-    { 0, 0, 40, 50 },
-    { 0, 0, 0, 70 },
-    { 0, 0, 0, 0 },
-  };
+bool isNumber(string s) {
+  return !s.empty() && find_if(s.begin(),  s.end(), [](unsigned char c) {
+    return !isdigit(c);
+  }) == s.end();
+}
 
-  vector<string> cities = { "Castle Black", "Winterfell", "Riverrun", "King's Landing" };
+vector<string> loadCities(istream& in) {
+  vector<string> cities;
+  string line;
+
+  if (getline(in, line)) {
+    int start, end = 0;
+    char token = ',';
+
+    while ((start = line.find_first_not_of(token, end)) != string::npos) {
+      end = line.find(token, start);
+      string city = line.substr(start, end - start);
+
+      if (!city.empty())
+        cities.push_back(city);
+    }
+  }
+
+  return cities;
+}
+
+vector<vector<int>> loadCosts(istream& in) {
+  vector<vector<int>> cost_matrix;
+  string line;
+
+  while (getline(in, line)) {
+    int start, end = 0;
+    char token = ',';
+    vector<int> row;
+
+    while ((start = line.find_first_not_of(token, end)) != string::npos) {
+      end = line.find(token, start);
+      string cost = line.substr(start, end - start);
+
+      if (isNumber(cost))
+        row.push_back(stoi(cost));
+    }
+
+    cost_matrix.push_back(row);
+  }
+
+  return cost_matrix;
+}
+
+int main() {
+  ifstream file = ifstream("../test_data/cities.csv");
+
+  if (!file) {
+    std::cerr << "Failed to open cities.csv" << endl;
+    return 1;
+  }
+
+  auto cities = loadCities(file);
+
+  file = ifstream("../test_data/costs.csv");
+
+  if (!file) {
+    std::cerr << "Failed to open costs.csv" << endl;
+    return 1;
+  }
+
+  auto cost_matrix = loadCosts(file);
+
   Graph graph = GraphBuilder::assemble(cost_matrix, cities);
 
   PathFinder* path_finder = new PathFinder(graph);
--- src/graph_builder.cpp
+++ src/graph_builder.cpp
@@ -8,7 +8,7 @@ Graph GraphBuilder::assemble(vector<vector<int>>& cost_matrix, vector<string>& c
 
   for (int i = 0; i < cost_matrix.size(); ++i) {
     for (int j = i + 1; j < cost_matrix[i].size(); ++j) {
-      if (cost_matrix[i][j] != 0)
+      if (cost_matrix[i][j] > 0)
         graph[cities[i]].push_back({ cities[j], cost_matrix[i][j] });
     }
   }
